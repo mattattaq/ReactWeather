1520465ded82a76a805654d11b7269c7243b3446
mattattaq <mattatttaq@gmail.com>
2016-12-28 09:17:11 -0500

    added jQuery and Foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,17 +1,15 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-  render: function () {
-    return (
-      <div>
-        <h1>Nav Component</h1>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
-        <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
-      </div>
-    );
-  }
-});
+var Nav = () => {
+  return (
+    <div>
+      <h1>Nav Component</h1>
+      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+      <IndexLink to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</IndexLink>
+      <IndexLink to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</IndexLink>
+    </div>
+  );
+}
 
 module.exports = Nav;
